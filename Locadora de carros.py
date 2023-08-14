0ece1d19eb545b1ce0e2455d3014b2ae22d83774
Wesley <wesley.soeiro@hotmail.com>
2023-08-14 17:35:36 -0300

    C2 - Correções do Programa, limpeza de Código.

--- Locadora de carros.py
+++ Locadora de carros.py
@@ -5,18 +5,121 @@ new_car = []
 new_value = []
 dev_car = []
 dev_value = []
-list_cars = {'Fiat Uno 4p flex': ' R$ 50 /dia',
-             'Gol 1.0 4p flez': ' R$ 60 /dia',
-             'Spin 8l 4p 1.7': ' R$ 100 /dia',
-             'Prisma 4p 1.4': ' R$ 90 /dia',
-             'Cruze 4p aut 2.0': ' R$ 200 /dia',
-             'Montana 2p 1.6': ' R$ 250 /dia',
-             'Hilux SW4 8l aut 3.0': ' R$ 800 /dia',
+list_cars = {'Fiat Uno 4p flex': '50',
+             'Gol 1.0 4p flez': '60',
+             'Spin 8l 4p 1.7': '100',
+             'Prisma 4p 1.4': '90',
+             'Cruze 4p aut 2.0': '200',
+             'Montana 2p 1.6': '250',
+             'Hilux SW4 8l aut 3.0': '800',
              }
 list_cars_alug = {}
 
+def finalizando_dev():
+    time.sleep(0.5)
+    print('\n===========')
+    print('\nEstamos processando a sua operação, por favor aguarde...')
+    print('\n===========')
+    time.sleep(2)
+    print('\n===========')
+    print('\nEstamos processando a sua operação, por favor aguarde...')
+    print('\n===========')
+    time.sleep(2)
+    print('\n===========')
+    print('\nPronto!')
+    print('\n===========')
+    time.sleep(1)
+    print('\n===========')
+    print(f'\nO carro {carro_dev[dev]} foi devolvido com sucesso!')
+    print('\n===========')
+    time.sleep(2)
+    print('\n===========')
+    print('\nObrigado por Utilizar nossos serviços!')
+    print('\n===========')
+    
+    
+    
+    
+def finalizando():
+    time.sleep(0.5)
+    print('\n===========')
+    print('\nEstamos processando a sua operação, por favor aguarde...')
+    print('\n===========')
+    time.sleep(2)
+    print('\n===========')
+    print('\nEstamos processando a sua operação, por favor aguarde...')
+    print('\n===========')
+    time.sleep(2)
+    print('\n===========')
+    print('\nPronto!')
+    print('\n===========')
+    time.sleep(1)
+    print('\n===========')
+    print(f'\nParabéns, você alugou o {carro_esc[car]}!')
+    print('\n===========')
+    time.sleep(2)
+    print('\n===========')
+    print('\nObrigado por Utilizar nossos serviços!')
+    print('\n===========')
+    time.sleep(2)
+
+
+def erro():
+    print('\n===========')
+    print('\n O valor selecionado é inválido, escolha novamente!')
+    print('\n===========')
+    time.sleep(2)
+    pass
+        
+        
+def mostrar_car():
+    i = 1
+    for carros, valor in list_cars.items():
+        print(f'[{i}] - {carros} - R${valor} /dia.')
+        i += 1
+        
+def dev_car():
+    d = 1
+    for devCar, devVlr in list_cars_alug.items():
+        print(f'[{d}]: {devCar} - R${devVlr} /dia.')
+        d += 1
+        
+        
+def aluguel():
+    print('\n===========')
+    print('Alugar - escolha entre nossas opções o carro que se encaixa na sua necessidade.')
+    print('===========\n')
+    i = 1
+    mostrar_car()
+    print('\n===========')
+    print('Antes de continuarmos nos diga, deseja continuar a operação?')
+    print('===========')
+    print('\n===========')
+    print('0 - Sair | 1 - Continuar')
+    op_user_alug = int(input())
+    time.sleep(0.5)
+    if op_user_alug == 1:
+        print("Você escolheu a opção Alugar?")
+        print("S / N")
+        al = input()
+        time.sleep(0.5)
+        if al == 'N' or al == 'n':
+            pass
+        elif al == 'S' or al == 's':
+            print('\n===========')
+            print('Alugar - Digite o número do carro que você deseja.')
+            print('===========\n')
+            i = 1
+            mostrar_car()
+            print('\n===========')
+            print('Qual será seu carro?')
+
+        else:
+            erro()
+    else:
+        erro()
+
 while True:
-    os.system('cls')
     print('\n===========')
     print('Bem vindo à locadora de carros')
     print('===========\n')
@@ -31,10 +134,7 @@ while True:
         print('\n===========')
         print('Portifólio - Estes são os modelos em nosso portifólio.')
         print('===========\n')
-        i = 1
-        for carro, valor in list_cars.items():
-            print(f'[{i}]: {carro} - {valor}')
-            i += 1
+        mostrar_car()
         print('\n===========')
         print('Antes de continuarmos nos diga, deseja continuar a operação?')
         print('\n===========')
@@ -42,190 +142,74 @@ while True:
         op_user_port = int(input())
         time.sleep(0.5)
         if op_user_port == 1:
-            print('\n===========')
-            print('Alugar - escolha entre nossas opções o carro que se encaixa na sua necessidade.')
-            print('===========\n')
-            i = 1
-            for carro, valor in list_cars.items():
-                print(f'[{i}]: {carro} - {valor}')
-                i += 1
-            print('\n===========')
-            print('Antes de continuarmos nos diga, deseja continuar a operação?')
-            print('===========')
-            print('\n===========')
-            print('0 - Sair | 1 - Continuar')
-            op_user_alug = int(input())
+            aluguel()
+            car = int(input()) - 1
             time.sleep(0.5)
-            if op_user_alug == 1:
-                print("Você escolheu a opção Alugar?")
-                print("S / N")
-                al = input()
-                time.sleep(0.5)
-                if al == 'N' or al == 'n':
-                    pass
-                elif al == 'S' or al == 's':
-                    print('\n===========')
-                    print('Alugar - Digite o número do carro que você deseja.')
-                    print('===========\n')
-                    i = 1
-                    for carro, valor in list_cars.items():
-                        print(f'[{i}]: {carro} - {valor}')
-                        i += 1
-                    print('\n===========')
-                    print('Antes de continuarmos nos diga, deseja continuar a operação?')
-                    print('===========')
-                    print('\n===========')
-                    print('Qual será seu carro?')
-                    car = int(input()) - 1
-                    time.sleep(0.5)
-                    carro_esc = list(list_cars.keys())
-                    valor_esc = list(list_cars.values())
-                    print(
-                        f'Você escolheu o {carro_esc[car]}? Ele esta custando {valor_esc[car]}, deseja finalizar a operação?')
-                    print("S / N")
-                    al_decis = input()
-                    time.sleep(0.5)
-                    if al_decis == 'N' or al_decis == 'n':
-                        pass
-                    elif al_decis == 'S' or al_decis == 's':
-                        time.sleep(0.5)
-                        print('\n===========')
-                        print('\nEstamos processando a sua operação, por favor aguarde...')
-                        print('\n===========')
-                        time.sleep(2)
-                        print('\n===========')
-                        print('\nEstamos processando a sua operação, por favor aguarde...')
-                        print('\n===========')
-                        time.sleep(2)
-                        print('\n===========')
-                        print('\nPronto!')
-                        print('\n===========')
-                        time.sleep(1)
-                        print('\n===========')
-                        print(f'\nParabéns, você alugou o {carro_esc[car]}!')
-                        print('\n===========')
-                        time.sleep(2)
-                        print('\n===========')
-                        print('\nObrigado por Utilizar nossos serviços!')
-                        print('\n===========')
-                        time.sleep(2)
-                        new_car.append(carro_esc[car])
-                        new_value.append(valor_esc[car])
-                        list_cars_alug = dict(zip(new_car, new_value))
-                        list_cars.pop(carro_esc[car], valor_esc[car])
-                        pass
-                    else:
-                        print('\n===========')
-                        print('\nO valor selecionado é inválido, escolha novamente!')
-                        print('\n===========')
-                        time.sleep(2)
-                else:
-                    print('\n===========')
-                    print('\nO valor selecionado é inválido, escolha novamente!')
-                    print('\n===========')
-                    time.sleep(2)
-            else:
-                print('\n===========')
-                print('\nO valor selecionado é inválido, escolha novamente!')
-                print('\n===========')
-                time.sleep(2)
-        else:
+            carro_esc = list(list_cars.keys())
+            valor_esc = list(list_cars.values())
+            print('\n===========\n')
+            print(f'Você escolheu o {carro_esc[car]}? Ele esta custando {valor_esc[car]}')
             print('\n===========')
-            print('\nO valor selecionado é inválido, escolha novamente!')
+            print('\n===========\n')
+            print(f'Por quantos dias você alugara este carro?')
+            print('\n===========\n')
+            dias = int(input())
             print('\n===========')
-            time.sleep(2)
-    elif op_user == 1:
-        print('\n===========')
-        print('Alugar - escolha entre nossas opções o carro que se encaixa na sua necessidade.')
-        print('===========\n')
-        i = 1
-        for carro, valor in list_cars.items():
-            print(f'[{i}]: {carro} - {valor}')
-            i += 1
-        print('\n===========')
-        print('Antes de continuarmos nos diga, deseja continuar a operação?')
-        print('===========')
-        print('0 - Sair | 1 - Continuar')
-        op_user_alug = int(input())
-        time.sleep(0.5)
-        if op_user_alug == 1:
-            print("Você escolheu a opção Alugar?")
+            print('\nO valor total da sua operação será de {} R$, deseja continuar a operação?'.format(int(dias) * int(valor_esc[car])))                
+            print('\n===========')
             print("S / N")
-            al = input()
+            al_decis = input()
             time.sleep(0.5)
-            if al == 'N' or al == 'n':
+            if al_decis == 'N' or al_decis == 'n':
                 pass
-            elif al == 'S' or al == 's':
-                print('\n===========')
-                print('Alugar - Digite o número do carro que você deseja.')
-                print('===========\n')
-                i = 1
-                for carro, valor in list_cars.items():
-                    print(f'[{i}]: {carro} - {valor}')
-                    i += 1
-                print('\n===========')
-                print('Qual será seu carro?')
-                car = int(input()) - 1
-                time.sleep(0.5)
-                carro_esc = list(list_cars.keys())
-                valor_esc = list(list_cars.values())
-                print(f'Você escolheu o {carro_esc[car]}? Ele esta custando {valor_esc[car]}, deseja finalizar a operação?')
-                print("S / N")
-                al_decis = input()
-                time.sleep(0.5)
-                if al_decis == 'N' or al_decis == 'n':
-                    pass
-                elif al_decis == 'S' or al_decis == 's':
-                    time.sleep(0.5)
-                    print('\n===========')
-                    print('\nEstamos processando a sua operação, por favor aguarde...')
-                    print('\n===========')
-                    time.sleep(2)
-                    print('\n===========')
-                    print('\nEstamos processando a sua operação, por favor aguarde...')
-                    print('\n===========')
-                    time.sleep(2)
-                    print('\n===========')
-                    print('\nPronto!')
-                    print('\n===========')
-                    time.sleep(1)
-                    print('\n===========')
-                    print(f'\nParabéns, você alugou o {carro_esc[car]}!')
-                    print('\n===========')
-                    time.sleep(2)
-                    print('\n===========')
-                    print('\nObrigado por Utilizar nossos serviços!')
-                    print('\n===========')
-                    time.sleep(2)
-                    new_car.append(carro_esc[car])
-                    new_value.append(valor_esc[car])
-                    list_cars_alug = dict(zip(new_car, new_value))
-                    list_cars.pop(carro_esc[car], valor_esc[car])
-                    pass
-                else:
-                    print('\n===========')
-                    print('\nO valor selecionado é inválido, escolha novamente!')
-                    print('\n===========')
-                    time.sleep(2)
+            elif al_decis == 'S' or al_decis == 's':
+                finalizando()
+                new_car.append(carro_esc[car])
+                new_value.append(valor_esc[car])
+                list_cars_alug = dict(zip(new_car, new_value))
+                list_cars.pop(carro_esc[car], valor_esc[car])
+                pass
             else:
-                print('\n===========')
-                print('\nO valor selecionado é inválido, escolha novamente!')
-                print('\n===========')
-                time.sleep(2)
+                erro()
+        elif op_user_port == 0:
+            pass
         else:
-            print('\n===========')
-            print('\nO valor selecionado é inválido, escolha novamente!')
-            print('\n===========')
-            time.sleep(2)
-
+            erro()
+    elif op_user == 1:
+        aluguel()
+        car = int(input()) - 1
+        time.sleep(0.5)
+        carro_esc = list(list_cars.keys())
+        valor_esc = list(list_cars.values())
+        print('\n===========\n')
+        print(f'Você escolheu o {carro_esc[car]}? Ele esta custando {valor_esc[car]}')
+        print('\n===========')
+        print('\n===========\n')
+        print(f'Por quantos dias você alugara este carro?')
+        print('\n===========\n')
+        dias = int(input())
+        print('\n===========')
+        print('\nO valor total da sua operação será de {} R$, deseja continuar a operação?'.format(int(dias) * int(valor_esc[car])))                
+        print('\n===========')
+        print("S / N")
+        al_decis = input()
+        time.sleep(0.5)
+        if al_decis == 'N' or al_decis == 'n':
+            pass
+        elif al_decis == 'S' or al_decis == 's':
+            finalizando()
+            new_car.append(carro_esc[car])
+            new_value.append(valor_esc[car])
+            list_cars_alug = dict(zip(new_car, new_value))
+            list_cars.pop(carro_esc[car], valor_esc[car])
+            pass
+        else:
+            erro()
     elif op_user == 2:
         print('\n===========')
         print('Devolver Carros - Qual carro deseja retornar?.')
         print('===========\n')
-        d = 1
-        for devCar, devVlr in list_cars_alug.items():
-            print(f'[{d}]: {devCar} - {devVlr}')
-            d += 1
+        dev_car()
         print('\n===========')
         print('Antes de continuarmos nos diga, deseja continuar?')
         print('===========')
@@ -236,11 +220,7 @@ while True:
             pass
         elif op_user_dev == 1:
             print(f'Você gostaria de devolver qual carro? Escolha na lista abaixo: ')
-            d = 1
-            for devCar, devVlr in list_cars_alug.items():
-                print(f'[{d}]: {devCar} - {devVlr}')
-                d += 1
-
+            dev_car()
             dev = int(input()) - 1
             time.sleep(0.5)
             carro_dev = list(list_cars_alug.keys())
@@ -252,42 +232,12 @@ while True:
             if dev_decis == 'N' or dev_decis == 'n':
                 pass
             elif dev_decis == 'S' or dev_decis == 's':
-                list_cars[carro_dev[dev]] = valor_dev[dev]
-                del list_cars_alug[carro_dev[dev]]
-                time.sleep(0.5)
-                print('\n===========')
-                print('\nEstamos processando a sua operação, por favor aguarde...')
-                print('\n===========')
-                time.sleep(2)
-                print('\n===========')
-                print('\nEstamos processando a sua operação, por favor aguarde...')
-                print('\n===========')
-                time.sleep(2)
-                print('\n===========')
-                print('\nPronto!')
-                print('\n===========')
-                time.sleep(1)
-                print('\n===========')
-                print(f'\nO carro {carro_dev[dev]} foi devolvido com sucesso!')
-                print('\n===========')
-                time.sleep(2)
-                print('\n===========')
-                print('\nObrigado por Utilizar nossos serviços!')
-                print('\n===========')
+                finalizando_dev()
                 time.sleep(2)
             else:
-                print('\n===========')
-                print('\nO valor selecionado é inválido, escolha novamente!')
-                print('\n===========')
-                time.sleep(2)
-                pass
+                erro()
+                
         else:
-            print('\n===========')
-            print('\nO valor selecionado é inválido, escolha novamente!')
-            print('\n===========')
-            time.sleep(2)
+            erro()
     else:
-        print('\n===========')
-        print('\nO valor selecionado é inválido, escolha novamente!')
-        print('\n===========')
-        time.sleep(2)
+        erro()
