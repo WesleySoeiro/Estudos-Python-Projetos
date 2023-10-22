78889aa3f2d5ee96319d18cadfdd3b7928adce63
WesleySoeiro <wesley.soeiro@hotmail.com>
2023-10-22 17:25:03 -0300

    C6 - Ajuste de linhas e arquivo debugger ipynb

--- Locadora.py
+++ Locadora.py
@@ -9,7 +9,6 @@ portfolio = {1:{'Fiat Uno 1.0 ': {'preco': 50, 'Ano': 2015, 'lugares': 5}},
              6:{'Corolla XEI 2.0 ': {'preco': 450, 'Ano': 2022, 'lugares': 5}},
              7:{'Hilux 3.0 SW4': {'preco': 800, 'Ano': 2023, 'lugares': 8}},
              8:{'Fiat Strada 1.4': {'preco': 370, 'Ano': 2012, 'lugares': 5}},}
-
 carros_info = {1:{'Fiat Uno 1.0 ': {'preco': 50, 'Ano': 2015, 'lugares': 5}},
              2:{'Palio 1.0 ': {'preco': 80, 'Ano': 2016, 'lugares': 5}},
              3:{'Onix 1.0 ': {'preco': 100, 'Ano': 2020, 'lugares': 5}},
@@ -67,31 +66,32 @@ def devolucao():
 def erro():
     print('','='*60,'\n O valor digitado está inválido, reentre com valores aceitos.\n','='*60)
 
-
-lista_de_escolhas = [x for x in range(1,9)]
+list_range = [x for x in range(0,8)]
 def info():
     print('Sobre qual carro deseja informações?\n\nEscolha o número do carro desejado entre as opções abaixo...\n\n')
     time.sleep(1)
     print('','='*44 ,f'\n Estes são todos os nossos carros disponíveis.\n', '='*44,'\n')
     for idx, marca in carros_info.items():
         print('[{}] - {} - R$ {},00 / dia'.format(idx, list(marca.keys())[0], list(marca.values())[0]['preco']))
-   
     esc_info = int(input()) - 1
-    while esc_info in lista_de_escolhas:
-        set_carros = list(carros_info.values())
-        nome_carro = []
-        ano_do_carro = []
-        assentos = []
-        nome_carro.append(list(list(carros_info.values())[esc_info])[0])
-        ano_do_carro.append(set_carros[esc_info][nome_carro[0]]['Ano'])
-        assentos.append(set_carros[esc_info][nome_carro[0]]['lugares'])
-        time.sleep(0.5)
-        print('\n','='*75,f'\n Voce escolheu o {nome_carro[0]} fabricado em {ano_do_carro[0]}, ele possui {assentos[0]} lugares.\n','='*75,'\n')
-        time.sleep(2)
-        print('\n','='*75,f'\n Você pode continuar buscando informações escolhendo outro número de carro...\n','='*75,'\n')
-        print(' 0 - Sair')
-        esc_info = int(input()) - 1
-        time.sleep(0.5)
+    while esc_info in list_range:
+            set_carros = list(carros_info.values())
+            nome_carro = []
+            ano_do_carro = []
+            assentos = []
+            nome_carro.append(list(list(carros_info.values())[esc_info])[0])
+            ano_do_carro.append(set_carros[esc_info][nome_carro[0]]['Ano'])
+            assentos.append(set_carros[esc_info][nome_carro[0]]['lugares'])
+            time.sleep(0.5)
+            print('\n','='*75,f'\n Voce escolheu o {nome_carro[0]} fabricado em {ano_do_carro[0]}, ele possui {assentos[0]} lugares.\n','='*75,'\n')
+            time.sleep(2)
+            print('\n','='*75,f'\n Você pode continuar buscando informações escolhendo outro número de carro...\n','='*75,'\n')
+            print(' 0 - Sair')
+            esc_info = int(input()) - 1
+            time.sleep(0.5)
+            continue
+
+    
 
 
 alpha = 0
@@ -136,4 +136,3 @@ while alpha == 0:
         erro()
         time.sleep(1)
         continue
-       
